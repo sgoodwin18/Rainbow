6183a2c32faf9a8099803c676ef02c8309a04287
Scott E Goodwin <scottgoodwin@protonmail.com>
2019-10-06 17:12:18 -0400

    Initial commit

---
+++ Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Scott Goodwin on 10/6/19.
+//  Copyright © 2019 Scott Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
