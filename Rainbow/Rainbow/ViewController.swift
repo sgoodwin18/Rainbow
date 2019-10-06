da9a0d48264397094cc742a82733d600519952ab
Scott E Goodwin <scottgoodwin@protonmail.com>
2019-10-06 17:48:10 -0400

    Buttons, Constraints, Background and IB Actions

--- Rainbow/Rainbow/ViewController.swift
+++ Rainbow/Rainbow/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = UIColor.green
+    
+    }
+    
+    @IBAction func orangeButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = UIColor.orange
+    
+    }
+    
+    @IBAction func blueButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+    
+    }
+    
+    @IBAction func magentaButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = UIColor.magenta
+    
+    }
+    
+    @IBAction func darkGrayButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = UIColor.darkGray
+    
+    }
+    
+    @IBAction func indigoButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = UIColor.init(displayP3Red: 88/255, green: 86/255, blue: 214/255, alpha: 1.0)
+    
+    }
+    
 }
 
